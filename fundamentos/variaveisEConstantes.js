9fa69a51a113e1d706018964dce20a6325a19bba
bertdev <herbertmatos10@hotmail.com>
2021-04-07 19:10:04 -0300

    adicionado o primero tópico de estudos

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,14 @@
+var idade = 10;
+let nome = "herbert";
+console.log(idade, nome);
+
+//var permite redeclaração no mesmo escopo
+var idade = 20;
+nome = "henrique";
+console.log(idade, nome);
+
+const ondeNasceu = "Teresina - PI";
+console.log(ondeNasceu);
+
+//as palavras reservadas let e var declaram uma váriavel
+//a palavra reservada const declara uma constante
